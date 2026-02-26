88a78d87397f80de3ac850ba198e5f533a39c9dc
albergm02 <alberto.g.m.0214@gmail.com>
2026-02-26 19:29:34 +0100

    feat: Añadido primeFlex y primeVue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Importaciones de PrimeVue */
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
+app.use(PrimeVue, { theme: { preset: Aura } })
 app.use(router)
 
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-    },
+    { path: '/', name: 'login', component: LoginView },
+    { path: '/register', name: 'register', component: RegisterView },
   ],
 })
 
