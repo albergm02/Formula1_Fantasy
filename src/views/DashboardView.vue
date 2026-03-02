46022c87a80d6e0fa737b3d98421fbc89298347f
albergm02 <alberto.g.m.0214@gmail.com>
2026-03-02 21:34:57 +0100

    Finalizado nav bar superior e inferior (TODO: Ruteos)

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -28,7 +28,7 @@ const cerrarSesion = async () => {
     class="min-h-screen flex flex-col justify-start bg-zinc-900 p-4 pb-24 items-center font-sans"
   >
     <header
-      class="w-full max-w-4xl flex items-center justify-between mb-8 mt-4 border-b border-zinc-700 pb-4"
+      class="flex w-full max-w-4xl flex items-center justify-between mb-8 mt-4 border-b border-zinc-700 pb-4"
     >
       <div class="flex items-center gap-4">
         <div
@@ -36,8 +36,8 @@ const cerrarSesion = async () => {
         >
           {{ usuario.iniciales }}
         </div>
-        <div>
-          <h2 class="text-xl font-bold text-white">{{ usuario.name }}</h2>
+        <div class="flex flex-col justify-center">
+          <h2 class="text-xl font-bold text-white uppercase">{{ usuario.name }}</h2>
           <p class="text-sm text-zinc-400 font-medium tracking-wide">
             Puntos: {{ usuario.puntos }} | Presupuesto: {{ usuario.presupuesto }}
           </p>
@@ -50,50 +50,52 @@ const cerrarSesion = async () => {
         @click="cerrarSesion"
       />
     </header>
-    // TODO
+    <!-- TODO -->
     <nav
       class="fixed bottom-0 left-0 w-full border-t border-zinc-700 p-4 flex justify-center gap-8"
     >
-      <Button
-        class="flex flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
-      >
-        <i class="pi pi-home" style="font-size: 1.5rem; color: white"></i>
-        <span class="text-[10px] text-white mt-1 font-bold tracking-widest whitespace-nowrap"
-          >INICIO</span
+      <div class="w-full max-w-4xl flex justify-around items-center">
+        <Button
+          class="flex-1 flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
         >
-      </Button>
-      <Button
-        class="flex flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
-      >
-        <i class="pi pi-chart-bar" style="font-size: 1.5rem; color: white"></i>
-        <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
-          >RANKING</span
+          <i class="pi pi-home" style="font-size: 1.5rem; color: white"></i>
+          <span class="text-[10px] text-white mt-1 font-bold tracking-widest whitespace-nowrap"
+            >INICIO</span
+          >
+        </Button>
+        <Button
+          class="flex-1 flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
         >
-      </Button>
-      <Button
-        class="flex flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
-      >
-        <i class="pi pi-warehouse" style="font-size: 1.5rem; color: white"></i>
-        <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
-          >GARAJE</span
+          <i class="pi pi-chart-bar" style="font-size: 1.5rem; color: white"></i>
+          <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
+            >RANKING</span
+          >
+        </Button>
+        <Button
+          class="flex-1 flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
         >
-      </Button>
-      <Button
-        class="flex flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
-      >
-        <i class="pi pi-bell" style="font-size: 1.5rem; color: white"></i>
-        <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
-          >AVISOS</span
+          <i class="pi pi-warehouse" style="font-size: 1.5rem; color: white"></i>
+          <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
+            >GARAJE</span
+          >
+        </Button>
+        <Button
+          class="flex-1 flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
         >
-      </Button>
-      <Button
-        class="flex flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
-      >
-        <i class="pi pi-cog" style="font-size: 1.5rem; color: white"></i>
-        <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
-          >AJUSTES</span
+          <i class="pi pi-bell" style="font-size: 1.5rem; color: white"></i>
+          <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
+            >AVISOS</span
+          >
+        </Button>
+        <Button
+          class="flex-1 flex-col items-center !p-2 !bg-transparent !border-none hover:!bg-zinc-800 !transition-colors"
         >
-      </Button>
+          <i class="pi pi-cog" style="font-size: 1.5rem; color: white"></i>
+          <span class="text-[10px] text-white mt-1 font-medium tracking-widest whitespace-nowrap"
+            >AJUSTES</span
+          >
+        </Button>
+      </div>
     </nav>
   </div>
 </template>
